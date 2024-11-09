ae4fc948d3acd04a1c93f1eb5bb87bfc9f70f814
anamulamin <aadelbanat8991@gmail.com>
2024-11-09 13:03:44 +0600

    compleded about vision

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import AboutMissionRoutes from "../app/modules/AboutMission/aboutMission.routes";
 import aboutUsRoutes from "../app/modules/AboutUs/aboutUs.routes";
+import aboutVisionRoutes from "../app/modules/AboutVission/aboutVision.routes";
 
 const routes = Router();
 // routes.use("/branches", BranchRoutes);
 routes.use("/about-mission", AboutMissionRoutes);
 routes.use("/about-us", aboutUsRoutes);
+routes.use("/about-vision", aboutVisionRoutes);
 
 
 export default routes;
