7af6f44b78f49741ea79966eab8112daffe452ad
anamulamin <aadelbanat8991@gmail.com>
2024-11-09 16:01:51 +0600

    blog completed

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import AboutMissionRoutes from "../app/modules/AboutMission/aboutMission.routes";
 import aboutUsRoutes from "../app/modules/AboutUs/aboutUs.routes";
-import aboutVisionRoutes from "../app/modules/AboutVission/aboutVision.routes";
 import bagBannerRoutes from "../app/modules/BagBanner/bagBanner.routes";
 import bannerRoutes from "../app/modules/Banner/banner.routes";
+import blogRoutes from "../app/modules/Blog/blog.routes";
+import aboutVisionRoutes from "../app/modules/AboutVision/aboutVision.routes";
 
 const routes = Router();
 // routes.use("/branches", BranchRoutes);
@@ -12,6 +13,7 @@ routes.use("/about-us", aboutUsRoutes);
 routes.use("/about-vision", aboutVisionRoutes);
 routes.use("/bag-banners", bagBannerRoutes );
 routes.use("/banners", bannerRoutes );
+routes.use("/blog", blogRoutes );
 
 
 export default routes;
