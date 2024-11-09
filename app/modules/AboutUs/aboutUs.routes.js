64fce9b676c47ed2b03623b4f490856ccf8e9552
anamulamin <aadelbanat8991@gmail.com>
2024-11-09 12:46:29 +0600

    completed about us

---
+++ app/modules/AboutUs/aboutUs.routes.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import { deleteAboutMission, getAboutMissionById, updateAboutMission } from '../AboutMission/aboutMission.controller';
+import { createAboutUs, getAboutUs } from './aboutUs.controller';
+
+const aboutUsRoutes = express.Router();
+
+// Route for getting the AboutMission by ID
+aboutUsRoutes.get('/', getAboutUs);
+aboutUsRoutes.get('/:id', getAboutMissionById);
+
+aboutUsRoutes.post('/', createAboutUs);
+// Route for updating the AboutMission by ID
+aboutUsRoutes.put('/:id', updateAboutMission);
+
+// Route for deleting the AboutMission by ID
+aboutUsRoutes.delete('/:id', deleteAboutMission);
+
+export default aboutUsRoutes;
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import AboutMissionRoutes from "../app/modules/AboutMission/aboutMission.routes";
+import aboutUsRoutes from "../app/modules/AboutUs/aboutUs.routes";
 
 const routes = Router();
 // routes.use("/branches", BranchRoutes);
 routes.use("/about-mission", AboutMissionRoutes);
+routes.use("/about-us", aboutUsRoutes);
 
 
 export default routes;
