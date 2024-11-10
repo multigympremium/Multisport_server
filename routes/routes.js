c4df2828599b4f94fe92d059418e4bb7f2bdf36e
anamulamin <aadelbanat8991@gmail.com>
2024-11-10 14:32:10 +0600

    subcategories

--- routes/routes.js
+++ routes/routes.js
@@ -22,6 +22,7 @@ import shippingPolicy from "../app/modules/ShippingPolicy/shippingPolicy.routes"
 import returnPolicy from "../app/modules/ReturnPolicy/returnPolicy.routes";
 import shoesBannerRoutes from "../app/modules/ShoesBanner/shoesBanner.routes";
 import socialLinkRoutes from "../app/modules/SocialLink/socialLink.routes";
+import subcategoryRoutes from "../app/modules/Subcategory/subcategory.routes";
 
 const routes = Router();
 // routes.use("/branches", BranchRoutes);
@@ -50,6 +51,7 @@ routes.use("/return_policy", returnPolicy);
 routes.use("/return_policy", returnPolicy);
 routes.use("/shoes-banners", shoesBannerRoutes);
 routes.use("/social-link", socialLinkRoutes);
+routes.use("/subcategories", subcategoryRoutes);
 
 
 export default routes;
