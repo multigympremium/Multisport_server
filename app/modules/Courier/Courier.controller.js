c44e10aa5a8b23fe152e66efe738a1f2809740a1
anamulamin <aadelbanat8991@gmail.com>
2024-11-29 00:00:10 +0600

    implement courier store

--- app/modules/Courier/Courier.controller.js
+++ app/modules/Courier/Courier.controller.js
@@ -54,3 +54,106 @@ export async function courierZones(req, res) {
     return null;
   }
 }
+export async function courierArea(req, res) {
+  const id = req.params.id;
+  const url = `${process.env.COURIER_URL}/aladdin/api/v1/zones/${id}/area-list`;
+
+  const accessToken = await courierAccessToken();
+
+  try {
+    const response = await axios.get(url,  {
+      headers: {
+        Authorization: `Bearer ${accessToken.access_token}`,
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+
+    console.log(response?.data, "response citeis")
+
+    
+
+     res.status(200).send({success: true, data: response?.data})
+
+
+  } catch (error) {
+    console.error("Error fetching access token:", error.response?.data || error.message);
+    return null;
+  }
+}
+
+
+export async function courierStoreGet(req, res) {
+  const id = req.params.id;
+  const url = `${process.env.COURIER_URL}/aladdin/api/v1/stores`;
+
+  const accessToken = await courierAccessToken();
+
+  try {
+    const response = await axios.get(url,  {
+      headers: {
+        Authorization: `Bearer ${accessToken.access_token}`,
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+
+    console.log(response?.data, "response citeis")
+
+    
+
+     res.status(200).send({success: true, data: response?.data})
+
+
+  } catch (error) {
+    console.error("Error fetching access token:", error.response?.data || error.message);
+    return null;
+  }
+}
+
+
+export const courierStoreCreate = async (req, res) => {
+  const requestData = req.body;
+  const url = `${process.env.COURIER_URL}/aladdin/api/v1/stores`; // Replace with actual base URL
+  // const accessToken = '<access_token>'; // Replace with your access token
+
+  const accessToken = await courierAccessToken();
+
+
+//   const requestData = {
+//     "name": "Enamul",
+//     "contact_name": "Enamul",
+//     "contact_number": "01761515810",
+//     "secondary_contact": "01961394375",
+//     "address": "B. Baria  Brahamanbaria Sadar  DAKSHIN MORAIL",
+//     "city_id": 1,
+//     "zone_id": 1,
+//     "area_id": 1,
+// }
+
+console.log(requestData, "requestData")
+  
+
+  try {
+    const response = await axios.post(url, requestData, {
+      headers: {
+        Authorization: `Bearer ${accessToken.access_token}`,
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    });
+
+    console.log(response.data, "response.data store")
+
+    res.status(200).json({
+      message: "Store created successfully",
+      store: response.data,
+    });
+  } catch (error) {
+    console.error('Error creating store:', error.response?.data || error.message);
+    res.status(500).json({
+      message: "Error creating store",
+      error: error.response?.data || error.message,
+    });
+  }
+};
--- app/modules/Courier/Courier.routes.js
+++ app/modules/Courier/Courier.routes.js
@@ -1,10 +1,13 @@
 
 import express from 'express';
-import {courierCities, courierZones} from './Courier.controller.js';
+import {courierArea, courierCities, courierStoreCreate, courierStoreGet, courierZones} from './Courier.controller.js';
 
 const courierRoutes = express.Router();
 
 courierRoutes.get('/cities', courierCities);
 courierRoutes.get('/zones/:id', courierZones);
+courierRoutes.get('/area/:id', courierArea);
+courierRoutes.get('/store', courierStoreGet);
+courierRoutes.post('/store', courierStoreCreate);
 
 export default courierRoutes;
